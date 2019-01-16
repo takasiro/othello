3d3e00ec200b5f5b6000890a0bdf8bac9e2958e2
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-01-16 14:52:03 +0900

    [add]置けるかどうか判断するプログラムの基盤作成
    AllJudge
    PieceJudge

---
+++ PieceJudge.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdio.h>
+#include "Global.h"
+#ifndef _PIECE_JUDGE_
+#define _PIECE_JUDGE_
+
+/************************************************
+関 数 名 :PieceJudge()
+処理内容 :駒が置けるかどうかの判定
+引    数 :  :配列のアドレス,player
+返 却 値 :無し
+備　　考 :
+*************************************************/
+int PieceJudge();
+
+#endif _PIECE_JUDGE_
