d19b3deabe3b4f58ce8d57a81ee5981c67f1edd0
takasiro <41959070+takasiro@users.noreply.github.com>
2019-01-16 15:30:03 +0900

    [update]pieceRverseの基盤の作成
    masuがglobalに書きました

--- PieceReverse.h
+++ PieceReverse.h
@@ -1,9 +1,14 @@
 #pragma once
 #ifndef  _PieceReverse_INCLUDE_
 #define _PieceReverse_INCLUDE_
-
-
-void PieceReverse(const int*, int, int, int);
+/************************************************
+関 数 名 :PieceReverse( int, int)
+処理内容 :挟んだ駒をひっくり返す
+引    数 :第一　　
+返 却 値 :無し
+備　　考 :なし
+*************************************************/
+void PieceReverse(int, int);
 
 
 #endif  _PieceReverse_INCLUDE_
