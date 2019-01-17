094d3d74b5a773c24eccac1b0e358a9e27c0844a
miyazaki44 <mhikaru0131@i.softbank.jp>
2019-01-17 14:49:13 +0900

    draw.cの改良

--- Draw.c
+++ Draw.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include"Global.h"
 #include"Windows.h"
+#include"Draw.h"
 
 void Draw() {
 	pt.x = (pt.x / 50) * 50;    //座標を枠内に入れる
 	pt.y = (pt.y / 50) * 50;    //　・・
-	SelectObject(hdc, hBrush[1]);  //白
-	Ellipse(hdc, pt.x, pt.y, pt.x + 50, pt.y + 50);
 
-
+	for (i = 1; i < 9; i++) { 
+		for (j = 1; j < 9; j++) {
+			if (masu[i][j] == BLACK) { //２次元配列を黒に
+				SelectObject(hdc, hBrush[1]);  //白
+				Ellipse(hdc, pt.x, pt.y, pt.x + 50, pt.y + 50);
+			}
+			else {
+				SelectObject(hdc, hBrush[2]);  //黒
+				Ellipse(hdc, pt.x, pt.y, pt.x + 50, pt.y + 50);
+			}
 
 	EndPaint(hWnd, &ps);
 	return 0;
--- Draw.h
+++ Draw.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <stdio.h>
 #include "Global.h"
+
+#ifndef _DRAW_FORESIGHT_
+#define _DRAW_FORESIGHT_
 /************************************************
 関 数 名 :Draw()(paint)
 処理内容 :表示するやつ
@@ -9,4 +12,6 @@
 備　　考 :無し
 *************************************************/
 
-void Draw();
+void Draw(); //返却値無しの為 void ?
+
+#endif _DRAW_FORESIGHT_
