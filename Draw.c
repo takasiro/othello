b9df34eedd23622b9e4ea223e366e72f50f7ff78
miyazaki44 <mhikaru0131@i.softbank.jp>
2019-01-17 13:50:05 +0900

    no message

--- Draw.c
+++ Draw.c
@@ -4,7 +4,7 @@
 
 void Draw() {
 	pt.x = (pt.x / 50) * 50;    //座標を枠内に入れる
-	pt.y = (pt.y / 50) * 50;
+	pt.y = (pt.y / 50) * 50;    //　・・
 	SelectObject(hdc, hBrush[1]);  //白
 	Ellipse(hdc, pt.x, pt.y, pt.x + 50, pt.y + 50);
 
