0d739b8bd33b4e5a2574c083082db6d9c7bd4468
miyazaki44 <mhikaru0131@i.softbank.jp>
2019-01-17 13:35:01 +0900

    作成中のマウス入力と描画です

---
+++ Draw.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include"Global.h"
+#include"Windows.h"
+
+void Draw() {
+	pt.x = (pt.x / 50) * 50;    //ç¿ïWÇògì‡Ç…ì¸ÇÍÇÈ
+	pt.y = (pt.y / 50) * 50;
+	SelectObject(hdc, hBrush[1]);  //îí
+	Ellipse(hdc, pt.x, pt.y, pt.x + 50, pt.y + 50);
+
+
+
+	EndPaint(hWnd, &ps);
+	return 0;
+}
