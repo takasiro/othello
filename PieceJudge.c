fe6649d9359c3ba28d1b93f06246a6f80b4d177a
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-01-17 15:09:17 +0900

    [update]8方向の判断
    一方向しか書いてません。

--- PieceJudge.c
+++ PieceJudge.c
@@ -9,12 +9,122 @@
 備　　考 :
 *************************************************/
 int PieceJudge() {
-	//マウスの座標からマス目を検出
-	//マスが0なら置ける
-	//0じゃないなら置けない
 
+	int mousex, mousey;
+	int Drct;
+	int i=0;
+	int tmpx, tmpy;
+	static int isDrctFlg = FALSE;
 
-	//0なら八方向を見る
+	//マウス座標より要素数を割り出し配列へ代入
+	mousex = pt.x / 50 % 8 + 1;
+	mousey = pt.y / 50 % 8 + 1;
+
+	tmpx = mousex;
+	tmpy = mousey;
+
+	//マスの中身を判断
+	if (masu[mousex][mousey] != EMPTY)
+	{
+		//0以外なら置けない
+		return FALSE;
+	}
+	//マスが0なら置けるかどうか調べる
+	else
+	{
+		while (1)
+		{
+
+			if (isDrctFlg == FALSE) {
+				Drct = drctArray[i];
+				i++;
+			}
+
+			switch (Drct)
+			{
+			case eN:
+				if (masu[mousex][mousey - 1] == player * REVERSE)
+				{
+					Drct = eN;
+					isDrctFlg = TRUE;
+					mousex = mousex;
+					mousey = mousey - 1;
+					if (masu[mousex][mousey - 1] == player) {
+						return TRUE;
+					}
+					else if (masu[mousex][mousey - 1] == player * REVERSE) {
+						mousex = mousex;
+						mousey = mousey - 1;
+					}
+					else {
+						mousex = tmpx;
+						mousey = tmpy;
+						break;
+					}
+				}
+				break;
+			case eNE:
+				if (masu[mousex + 1][mousey - 1] == player * REVERSE)
+				{
+					Drct = eNE;
+					mousex = mousex + 1;
+					mousey = mousey - 1;
+				}
+				break;
+			case eE:
+				if (masu[mousex + 1][mousey] == player * REVERSE)
+				{
+					Drct = eE;
+					mousex = mousex + 1;
+					mousey = mousey;
+				}
+				break;
+			case eSE:
+				if (masu[mousex + 1][mousey + 1] == player * REVERSE)
+				{
+					Drct = eSE;
+					mousex = mousex + 1;
+					mousey = mousey + 1;
+				}
+				break;
+			case eS:
+				if (masu[mousex][mousey + 1] == player * REVERSE)
+				{
+					Drct = eS;
+					mousex = mousex;
+					mousey = mousey + 1;
+				}
+				break;
+			case eSW:
+				if (masu[mousex - 1][mousey + 1] == player * REVERSE)
+				{
+					Drct = eSW;
+					mousex = mousex - 1;
+					mousey = mousey + 1;
+				}
+				break;
+			case eW:
+				if (masu[mousex - 1][mousey] == player * REVERSE)
+				{
+					Drct = eW;
+					mousex = mousex - 1;
+					mousey = mousey;
+				}
+				break;
+			case eNW:
+				if (masu[mousex - 1][mousey - 1] == player * REVERSE)
+				{
+					Drct = eNW;
+					mousex = mousex - 1;
+					mousey = mousey - 1;
+				}
+				break;
+			default:
+				break;
+			}  //switch文終了
+		}  //while文終了
+	}  //else終了
+
 	//方向の先が0かplayerならコンティニュー
 	//方向の先がplayer * REVERSE なら更にその方向の次のマスを見る
 	//先のマスがplayer * REVERSE なら繰り返す
