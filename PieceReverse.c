f2d22f6f54ffefa959234baf95795c4bcabce965
takasiro <41959070+takasiro@users.noreply.github.com>
2019-01-24 11:23:03 +0900

    [update]全方向のreverseの実装？
    出来ているはず

--- PieceReverse.c
+++ PieceReverse.c
@@ -9,12 +9,14 @@ void PieceReverse() {
 	int mousey;			// マウスのx座標から計算したマス目
 	int mousex;			// マウスのy座標から計算したマス目
 	int Drct;			// 方向(Direction)を保存する
-static int mouceStarageX;	// マウスのx座標を保存する
-static	int mouceStarageY;	// マウスのy座標を保存する
+	static int mouceStarageX;	// マウスのx座標を保存する
+	static	int mouceStarageY;	// マウスのy座標を保存する
 	static int isDrctFlg = FALSE;	// 方向が定まっているか >> TRUE:定まっている FALSE:定まっていない
 	static int isFirstFlg = TRUE;	// 最初かどうか >> TRUE:最初 FALSE:最初じゃない
-	static int k = 0;
+
 	int i;
+	int j;
+	static int k = 0;
 	//マウス座標より要素数を割り出し配列へ代入
 	mousey = pt.y;
 	mousex = pt.x;
@@ -60,285 +62,265 @@ static	int mouceStarageY;	// マウスのy座標を保存する
 		{
 
 		case eN:
-			if (masu[mousey-1][mousex] == player * REVERSE)
+			if (masu[mousey - 1][mousex] == player * REVERSE)
 			{
 				//masu[mousey][mousex - 1] = player * REVERSE;
 				Drct = eN;
 				isDrctFlg = TRUE;
 			}
-				/*mousey = mousey;
-				mousex = mousex - 1;*/
+			/*mousey = mousey;
+			mousex = mousex - 1;*/
 
-				if (masu[mousey-1][mousex] == player * REVERSE)
-				{
-					//masu[mousey][mousex - 1] = player * REVERSE;
-					mousey = mousey-1;
-					mousex = mousex ;
-				}
-				else if (masu[mousey-1][mousex] == player) {
-					for (i = mousey; i < mouceStarageX+1; i++) {
-					if (masu[i][mousex] != player) masu[i][mousex] =player ;
-				}
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
-				else
-				{
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
-			
-			break;
-
-		case eNE:
-			if (masu[mousey + 1][mousex - 1] == player * REVERSE)
+			if (masu[mousey - 1][mousex] == player * REVERSE)
 			{
-				masu[mousey + 1][mousex - 1] = player * REVERSE;
-				Drct = eNE;
-				isDrctFlg = TRUE;
-				mousey = mousey + 1;
-				mousex = mousex - 1;
-
-				if (masu[mousey + 1][mousex - 1] == player * REVERSE)
-				{
-					//masu[mousey + 1][mousex - 1] = player * REVERSE;
-					mousey = mousey + 1;
-					mousex = mousex - 1;
+				//masu[mousey][mousex - 1] = player * REVERSE;
+				mousey = mousey - 1;
+				mousex = mousex;
+			}
+			else if (masu[mousey - 1][mousex] == player) {
+				for (i = mousey; i < mouceStarageY + 1; i++) {
+					if (masu[i][mousex] != player) masu[i][mousex] = player;
 				}
-				else if (masu[mousey + 1][mousex - 1] == player) {
-					for (int i = mousex + 1; i < mouceStarageY; i++) {
-						for (int j = mousey - 1; j > mouceStarageX; j--) {
-							masu[j][i] = player * REVERSE;
-						}
-					}
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
-				else
-				{
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
+				isDrctFlg = FALSE;
+				isFirstFlg = FALSE;
+				/*mousey = mouceStarageX;
+				mousex = mouceStarageY;*/
+				break;
 			}
 			else
 			{
+				isDrctFlg = FALSE;
 				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
+			}
+
+			break;
+
+		case eNE:
+			if (masu[mousey - 1][mousex + 1] == player * REVERSE)
+			{
+				//masu[mousey][mousex - 1] = player * REVERSE;
+				Drct = eNE;
+				isDrctFlg = TRUE;
+			}
+			/*mousey = mousey;
+			mousex = mousex - 1;*/
+
+			if (masu[mousey - 1][mousex + 1] == player * REVERSE)
+			{
+				//masu[mousey][mousex - 1] = player * REVERSE;
+				mousey = mousey - 1;
+				mousex = mousex + 1;
+			}
+			else if (masu[mousey - 1][mousex + 1] == player) {
+				for (i = mousey; i < mouceStarageY + 1; i++) {
+					for (j = mousex; j > mouceStarageX - 1; j--) {
+						if (masu[i][j] != player) masu[i][j] = player;
+					}
+				}
+				isDrctFlg = FALSE;
+				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
+			}
+			else
+			{
+				isDrctFlg = FALSE;
+				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
 			}
 			break;
 
 
 		case eE:
-			if (masu[mousey + 1][mousex] == player * REVERSE)
+			if (masu[mousey][mousex + 1] == player * REVERSE)
 			{
-				masu[mousey + 1][mousex] = player * REVERSE;
+				//masu[mousey][mousex - 1] = player * REVERSE;
 				Drct = eE;
 				isDrctFlg = TRUE;
-				mousey = mousey + 1;
-				mousex = mousex;
-				if (masu[mousey + 1][mousex] == player * REVERSE)
-				{
-					masu[mousey + 1][mousex] = player * REVERSE;
-					mousey = mousey + 1;
-					mousex = mousex;
+			}
+			/*mousey = mousey;
+			mousex = mousex - 1;*/
+
+			if (masu[mousey][mousex + 1] == player * REVERSE)
+			{
+				//masu[mousey][mousex - 1] = player * REVERSE;
+				mousey = mousey;
+				mousex = mousex + 1;
+			}
+			else if (masu[mousey][mousex + 1] == player) {
+				for (j = mousex; j > mouceStarageX - 1; j--) {
+					if (masu[mousex][j] != player) masu[mousex][j] = player;
 				}
-				else if (masu[mousey + 1][mousex] == player) {
-						for (int i = mousey - 1; i > mouceStarageX; i--) {
-							masu[i][mousex] = player * REVERSE;
-						}
-						isDrctFlg = FALSE;
-						isFirstFlg = FALSE;
-						mousey = mouceStarageX;
-						mousex = mouceStarageY;
-						break;
-					}
-				else
-				{
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
+				isDrctFlg = FALSE;
+				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
 			}
 			else
 			{
+				isDrctFlg = FALSE;
 				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
 			}
 			break;
-
 
 		case eSE:
 			if (masu[mousey + 1][mousex + 1] == player * REVERSE)
 			{
-				masu[mousey + 1][mousex + 1] = player * REVERSE;
+				//masu[mousey][mousex - 1] = player * REVERSE;
 				Drct = eSE;
 				isDrctFlg = TRUE;
+			}
+			/*mousey = mousey;
+			mousex = mousex - 1;*/
+
+			if (masu[mousey + 1][mousex + 1] == player * REVERSE)
+			{
+				//masu[mousey][mousex - 1] = player * REVERSE;
 				mousey = mousey + 1;
 				mousex = mousex + 1;
-				if (masu[mousey + 1][mousex + 1] == player * REVERSE)
-				{
-					masu[mousey + 1][mousex + 1] = player * REVERSE;
-					mousey = mousey + 1;
-					mousex = mousex + 1;
-				}
-				else if (masu[mousey + 1][mousex +1] == player) {
-					for (int i = mousex - 1; i < mouceStarageY; i++) {
-						for (int j = mousey - 1; j > mouceStarageX; j--) {
-							masu[j][i] = player * REVERSE;
-						}
+			}
+			else if (masu[mousey + 1][mousex + 1] == player) {
+				for (i = mousey; i > mouceStarageY - 1; i--) {
+					for (j = mousex; j > mouceStarageX - 1; j--) {
+						if (masu[i][j] != player) masu[i][j] = player;
 					}
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
 				}
-				else
-				{
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
+				isDrctFlg = FALSE;
+				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
 			}
 			else
 			{
+				isDrctFlg = FALSE;
 				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
 			}
 			break;
 
 
 		case eS:
-			if (masu[mousey][mousex + 1] == player * REVERSE)
+			if (masu[mousey + 1][mousex] == player * REVERSE)
 			{
-				masu[mousey][mousex + 1] = player * REVERSE;
+				//masu[mousey][mousex - 1] = player * REVERSE;
 				Drct = eS;
 				isDrctFlg = TRUE;
-				mousey = mousey;
-				mousex = mousex + 1;
-				if (masu[mousey][mousex + 1] == player * REVERSE)
-				{
-					masu[mousey][mousex + 1] = player * REVERSE;
-					mousey = mousey;
-					mousex = mousex + 1;
+			}
+			/*mousey = mousey;
+			mousex = mousex - 1;*/
+
+			if (masu[mousey + 1][mousex] == player * REVERSE)
+			{
+				//masu[mousey][mousex - 1] = player * REVERSE;
+				mousey = mousey + 1;
+				mousex = mousex;
+			}
+			else if (masu[mousey + 1][mousex] == player) {
+				for (i = mousey; i > mouceStarageY - 1; i--) {
+					if (masu[i][mousex] != player) masu[i][mousex] = player;
 				}
-				else if (masu[mousey][mousex - 1] == player) {
-					for (int i = mousex + 1; i < mouceStarageY; i++) {
-							masu[mousey][i] = player * REVERSE;
-						}
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
-				else
-				{
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
+				isDrctFlg = FALSE;
+				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
 			}
 			else
 			{
+				isDrctFlg = FALSE;
 				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
 			}
 			break;
 
 
 		case eSW:
-			if (masu[mousey - 1][mousex + 1] == player * REVERSE)
+			if (masu[mousey + 1][mousex - 1] == player * REVERSE)
 			{
-				masu[mousey - 1][mousex + 1] = player * REVERSE;
+				//masu[mousey][mousex - 1] = player * REVERSE;
 				Drct = eSW;
 				isDrctFlg = TRUE;
-				mousey = mousey - 1;
-				mousex = mousex + 1;
-				if (masu[mousey - 1][mousex + 1] == player * REVERSE)
-				{
-					masu[mousey - 1][mousex + 1] = player * REVERSE;
-					mousey = mousey - 1;
-					mousex = mousex + 1;
-				}
-				else if (masu[mousey - 1][mousex +1] == player) {
-					for (int i = mousex - 1; i > mouceStarageY; i++) {
-						for (int j = mousey + 1; j < mouceStarageX; j--) {
-							masu[j][i] = player * REVERSE;
-						}
-					}
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
-				else
-				{
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
 			}
-			else
+			/*mousey = mousey;
+			mousex = mousex - 1;*/
+
+			if (masu[mousey + 1][mousex - 1] == player * REVERSE)
 			{
-				isFirstFlg = FALSE;
+				//masu[mousey][mousex - 1] = player * REVERSE;
+				mousey = mousey + 1;
+				mousex = mousex - 1;
 			}
+			else if (masu[mousey + 1][mousex - 1] == player) {
+				for (i = mousey; i > mouceStarageY - 1; i--) {
+					for (j = mousex; j < mouceStarageX + 1; j ++ ) {
+						if (masu[i][j] != player) masu[i][j] = player;
+					}
+				}
+			isDrctFlg = FALSE;
+			isFirstFlg = FALSE;
+			mousey = mouceStarageX;
+			mousex = mouceStarageY;
 			break;
+		}
+		else
+		{
+			isDrctFlg = FALSE;
+			isFirstFlg = FALSE;
+			mousey = mouceStarageX;
+			mousex = mouceStarageY;
+			break;
+		}
+		break;
 
 
 		case eW:
-			if (masu[mousey - 1][mousex] == player * REVERSE)
+			if (masu[mousey][mousex - 1] == player * REVERSE)
 			{
-				masu[mousey - 1][mousex] = player * REVERSE;
+				//masu[mousey][mousex - 1] = player * REVERSE;
 				Drct = eW;
 				isDrctFlg = TRUE;
-				mousey = mousey - 1;
-				mousex = mousex;
-				if (masu[mousey - 1][mousex] == player * REVERSE)
-				{
-					masu[mousey - 1][mousex] = player * REVERSE;
-					mousey = mousey - 1;
-					mousex = mousex;
+			}
+			/*mousey = mousey;
+			mousex = mousex - 1;*/
+
+			if (masu[mousey][mousex - 1] == player * REVERSE)
+			{
+				//masu[mousey][mousex - 1] = player * REVERSE;
+				mousey = mousey;
+				mousex = mousex + 1;
+			}
+			else if (masu[mousey][mousex - 1] == player) {
+				for (j = mousex; j < mouceStarageX + 1; j++) {
+					if (masu[mousey][j] != player) masu[mousey][j] = player;
 				}
-				else if (masu[mousey - 1][mousex] == player) {
-						for (int j = mousey + 1; j < mouceStarageX; j--) {
-							masu[j][mousex] = player * REVERSE;
-						}
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
-				else
-				{
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
+				isDrctFlg = FALSE;
+				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
 			}
 			else
 			{
+				isDrctFlg = FALSE;
 				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
 			}
 			break;
 
@@ -346,53 +328,50 @@ static	int mouceStarageY;	// マウスのy座標を保存する
 		case eNW:
 			if (masu[mousey - 1][mousex - 1] == player * REVERSE)
 			{
-				masu[mousey - 1][mousex - 1] = player * REVERSE;
+				//masu[mousey][mousex - 1] = player * REVERSE;
 				Drct = eNW;
 				isDrctFlg = TRUE;
+			}
+			/*mousey = mousey;
+			mousex = mousex - 1;*/
+
+			if (masu[mousey - 1][mousex - 1] == player * REVERSE)
+			{
+				//masu[mousey][mousex - 1] = player * REVERSE;
 				mousey = mousey - 1;
 				mousex = mousex - 1;
-				if (masu[mousey - 1][mousex - 1] == player * REVERSE)
-				{
-					masu[mousey - 1][mousex - 1] = player * REVERSE;
-					mousey = mousey;
-					mousex = mousex - 1;
-				}
-				else if (masu[mousey- 1][mousex - 1] == player) {
-					for (int i = mousex - 1; i > mouceStarageY; i++) {
-						for (int j = mousey - 1; j > mouceStarageX; j--) {
-							masu[j][i] = player * REVERSE;
-						}
+			}
+			else if (masu[mousey - 1][mousex - 1] == player) {
+				for (i = mousey; i > mouceStarageY - 1; i--) {
+					for (j = mousex; j > mouceStarageX - 1; j--) {
+						if (masu[i][j] != player) masu[i][j] = player;
 					}
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
 				}
-				else
-				{
-					isDrctFlg = FALSE;
-					isFirstFlg = FALSE;
-					mousey = mouceStarageX;
-					mousex = mouceStarageY;
-					break;
-				}
+				isDrctFlg = FALSE;
+				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
 			}
 			else
 			{
+				isDrctFlg = FALSE;
 				isFirstFlg = FALSE;
+				mousey = mouceStarageX;
+				mousex = mouceStarageY;
+				break;
 			}
 			break;
 
 
-		}  //switch文終了
+	}  //switch文終了
 
-		// どの方向も置けないなら
-		if (k == 8)
-		{
-			k = 0;
-			break;
-		}
-	}  //while文終了
+	// どの方向も置けないなら
+	if (k == 8)
+	{
+		k = 0;
+		break;
+	}
+}  //while文終了
 
 }
